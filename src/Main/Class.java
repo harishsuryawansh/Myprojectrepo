7984f0b0d8d837001861de26f872769426401346
harish suryawanshi <141421801+harishsuryawansh@users.noreply.github.com>
2025-08-08 21:36:09 +0530

    commit push.

---
+++ src/Main/Class.java
@@ -0,0 +1,18 @@
+package Main;
+
+import java.util.Scanner;
+
+public class Class {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("enter two number ");
+		int a= in.nextInt();
+		int b = in.nextInt();
+		int c = a+b;
+		System.out.println("the sum of a and b "+c);
+		 
+		
+		
+	}
+
+}
