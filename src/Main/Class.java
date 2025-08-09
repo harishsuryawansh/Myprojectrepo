c70d620eb1e05b87ea191c5a0057c756be3b111b
harish suryawanshi <141421801+harishsuryawansh@users.noreply.github.com>
2025-08-09 08:02:47 +0530

    update some logic : converting into integer to float

--- src/Main/Class.java
+++ src/Main/Class.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Class {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.println("enter two number ");
-		int a= in.nextInt();
-		int b = in.nextInt();
-		int c = a+b;
+		System.out.println("enter two number and any float number  ");
+		double a= in.nextInt();
+		double b = in.nextInt();
+		double  c = a+b;
 		System.out.println("the sum of a and b "+c);
 		 	
 	}
